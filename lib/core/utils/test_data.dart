bf2810fe40be06e51a988e9661fb998a717dd39d
binaerverkehr <binaerverkehr@gmail.com>
2024-04-15 15:35:00 +0200

    Added test data

---
+++ lib/core/utils/test_data.dart
@@ -0,0 +1,126 @@
+final List<Map<String, dynamic>> kTestData = [
+  {
+    "id": 1,
+    "name": "Capitals",
+    "cards": [
+      {
+        "deckId": 1,
+        "front": "Germany",
+        "back": "Berlin",
+      },
+      {
+        "deckId": 1,
+        "front": "France",
+        "back": "Paris",
+      },
+      {
+        "deckId": 1,
+        "front": "Italy",
+        "back": "Rome",
+      },
+      {
+        "deckId": 1,
+        "front": "Spain",
+        "back": "Madrid",
+      },
+      {
+        "deckId": 1,
+        "front": "Portugal",
+        "back": "Lisbon",
+      },
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Programming",
+    "cards": [
+      {
+        "deckId": 2,
+        "front": "What is Flutter?",
+        "back": "A framework for building mobile applications",
+      },
+      {
+        "deckId": 2,
+        "front": "What is Dart?",
+        "back": "The programming language used to build Flutter apps",
+      },
+      {
+        "deckId": 2,
+        "front": "What is a Widget?",
+        "back": "The basic building block of a Flutter app",
+      },
+      {
+        "deckId": 2,
+        "front": "What is a Stateful Widget?",
+        "back": "A widget that has a mutable state",
+      },
+      {
+        "deckId": 2,
+        "front": "What is a Stateless Widget?",
+        "back": "A widget that does not have a mutable state",
+      },
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Math",
+    "cards": [
+      {
+        "deckId": 3,
+        "front": "What is 2 + 2?",
+        "back": "4",
+      },
+      {
+        "deckId": 3,
+        "front": "What is 3 * 3?",
+        "back": "9",
+      },
+      {
+        "deckId": 3,
+        "front": "What is 10 / 2?",
+        "back": "5",
+      },
+      {
+        "deckId": 3,
+        "front": "What is 5 - 3?",
+        "back": "2",
+      },
+      {
+        "deckId": 3,
+        "front": "What is 8 % 3?",
+        "back": "2",
+      },
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Science",
+    "cards": [
+      {
+        "deckId": 4,
+        "front": "What is the boiling point of water?",
+        "back": "100°C",
+      },
+      {
+        "deckId": 4,
+        "front": "What is the freezing point of water?",
+        "back": "0°C",
+      },
+      {
+        "deckId": 4,
+        "front": "What is the atomic number of oxygen?",
+        "back": "8",
+      },
+      {
+        "deckId": 4,
+        "front": "What is the chemical symbol for gold?",
+        "back": "Au",
+      },
+      {
+        "deckId": 4,
+        "front": "What is the largest planet in our solar system?",
+        "back": "Jupiter",
+      },
+    ]
+  },
+];
