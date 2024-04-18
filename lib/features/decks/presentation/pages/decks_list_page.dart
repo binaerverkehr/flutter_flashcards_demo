54584adb116da700ae516287b4aaa1c196f8f4e4
binaerverkehr <binaerverkehr@gmail.com>
2024-04-18 10:34:00 +0200

    Refactoring

--- lib/features/decks/presentation/pages/decks_list_page.dart
+++ lib/features/decks/presentation/pages/decks_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lernkarten_app_test/features/decks/presentation/widgets/decks_grid.dart';
+import 'package:flutter_lernkarten_app_test/core/utils/test_data.dart';
+import 'package:flutter_lernkarten_app_test/features/decks/presentation/pages/decks_create_page.dart';
+import 'package:flutter_lernkarten_app_test/features/decks/presentation/widgets/selectable_deck_tile.dart';
 
 class DecksListPage extends StatelessWidget {
   const DecksListPage({
@@ -8,27 +10,61 @@ class DecksListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final decks = kTestDeckList;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Decks'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 24.0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 24.0),
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Text(' Welcome ', style: TextStyle(fontSize: 32)),
                 Text("User", style: TextStyle(fontSize: 32, color: Colors.purple, fontWeight: FontWeight.bold)),
               ],
             ),
-            Row(
+            const Row(
               children: [
                 Text(' to your flashcard decks!', style: TextStyle(fontSize: 32)),
               ],
             ),
-            SizedBox(height: 20),
-            Expanded(child: DecksGrid()),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                itemCount: decks.length + 1,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) {
+                  if (index == 0 || decks.isEmpty) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const DecksCreatePage(),
+                          ));
+                        },
+                        child: const Card(
+                          elevation: 0,
+                          child: Center(
+                            child: Icon(Icons.add, size: 64),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: SelectableDeckTile(deck: decks[index - 1]),
+                    );
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
